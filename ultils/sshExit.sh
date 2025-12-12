d955dbb6d37683e40976fbc1bd37d601f0398cfb
Shawn-Summer <362847502@qq.com>
2025-12-12 08:28:45 +0800

    update

--- ultils/sshExit.sh
+++ ultils/sshExit.sh
@@ -3,7 +3,7 @@ onExit(){
   [ ! -f "$HOME/xdgconfig.enc" ] && echo "???? xdgconfig.enc missing !" > "$XDG_CONFIG_HOME/logs/exit"
   myencrypt "$XDG_CONFIG_HOME" "$SSH_XDG_CONFIG_PASSWD" "$HOME/xdgconfig.enc"
   [ ! -f "$HOME/xdgconfig.enc" ] && echo "[error] xdgconfig.enc generation fails !" > "$XDG_CONFIG_HOME/logs/exit"
-  rm -rf "$XDG_CONFIG_HOME"
+  mv "$XDG_CONFIG_HOME" "$XDG_CONFIG_HOME.bak"
 
 }
 
