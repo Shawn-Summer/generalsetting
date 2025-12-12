a8ed6e5fe2ed1b456100f21d00169128afb7e8c3
Shawn-Summer <362847502@qq.com>
2025-12-12 20:24:22 +0800

    update

--- ultils/sshExit.sh
+++ ultils/sshExit.sh
@@ -1,7 +1,7 @@
 mylogout(){
-  echo "log out !!!" > "$XDG_CONFIG_HOME/logs/exit"
-  myencrypt "$XDG_CONFIG_HOME" "$SSH_XDG_CONFIG_PASSWD" "$HOME/xdgconfig.enc" >> "$XDG_CONFIG_HOME/logs/exit" 2>&1
-  [ ! -f "$HOME/xdgconfig.enc" ] && echo "[error] xdgconfig.enc generation fails !" >> "$XDG_CONFIG_HOME/logs/exit"
+  echo "log out !!!" 
+  myencrypt "$XDG_CONFIG_HOME" "$SSH_XDG_CONFIG_PASSWD" "$HOME/xdgconfig.enc"
+  [ ! -f "$HOME/xdgconfig.enc" ] && echo "[error] xdgconfig.enc generation fails !" 
   [ -f "$HOME/xdgconfig.enc" ] && rm -rf "$XDG_CONFIG_HOME"
 
 }
